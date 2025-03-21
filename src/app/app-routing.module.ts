2a95796247e8e83a89d795600abe6697704f5f17
x-jirox <correalenin045gmail.com>
2025-03-20 20:35:00 -0500

    medical-appoiment new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},  // Página de inicio
+  {path: 'login', component: LoginComponent}, // Pagina de Login
+  {path: 'register', component: RegisterComponent}, // Pagina de Registro
+  {path: '404', component: NotFoundComponent}, // Página 404
+  {path: '**', redirectTo: '/404'} // Redirige a la página 404 si no encuentra la ruta
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
