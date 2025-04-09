3a11ea097d3f91c5c9f27a0531020ca222eca9ac
x-jirox <correalenin045gmail.com>
2025-04-08 20:45:48 -0500

    added new tasks to review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,39 @@ import { AuthService } from './shared/auth.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
+})export class AppComponent implements OnInit {
   isLoggedIn = false;
   isSidebarOpen = true;
   role: string | null = null;
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit() {
+    this.subscribeToRouterEvents();
+  }
+
+  private subscribeToRouterEvents() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.isLoggedIn = this.authService.isLoggedIn();
-        this.role = this.authService.getRole(); // Actualizar el rol
-
-        if (!this.isLoggedIn) {
-          this.isSidebarOpen = false;
-        }
+        this.updateAuthState();
       }
     });
+  }
+
+  private updateAuthState() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.role = this.authService.getRole();
+
+    if (!this.isLoggedIn) {
+      this.isSidebarOpen = false;
+    }
   }
 
   toggleSidebar(isOpen: boolean) {
     this.isSidebarOpen = isOpen;
   }
 }
+
 
 /*Son buenas practicas?
 revisar el uso de los modulos lazy loading, para que no se carguen todos los modulos al inicio, sino solo los necesarios.
