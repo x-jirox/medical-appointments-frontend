ea395a5714bc420a544844661c208a807c5a4759
x-jirox <correalenin045@gmail.com>
2025-05-23 10:56:10 -0500

    tst correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { AuthService } from './shared/authentication/auth.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,32 +9,3 @@ export class AppComponent  {
 
 }
 
-/*Investigar acerca de sokets para mensajes 
-
-Qué debo aprender para usar Socket.IO en Angular
-✅ WebSockets / Socket.IO
-Cómo funciona la comunicación en tiempo real.
-
-Métodos básicos: connect, disconnect, on, emit.
-
-✅ Socket.IO en Angular
-Uso de socket.io-client.
-
-Crear un servicio Angular para manejar la conexión.
-
-Escuchar y emitir eventos desde componentes.
-
-✅ Autenticación con JWT
-Obtener y guardar el token al hacer login.
-
-Enviar el token en la conexión del socket.
-
-Validar el token en el servidor usando io.use().
-
-✅ Backend con Socket.IO
-Inicializar el servidor Socket.IO.
-
-Validar el token en el handshake.
-
-Identificar al usuario conectado y manejar sus eventos.
-*/
