7bd4e454757c955f651c45263580de9fc968d11d
x-jirox <correalenin045@gmail.com>
2025-05-23 10:36:43 -0500

    add doctors register

--- src/app/features/user/admin/admin.module.ts
+++ src/app/features/user/admin/admin.module.ts
@@ -1,17 +1,13 @@
 // src/app/auth/auth.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { DoctoresComponent } from './components/doctores/doctores.component';
-
 
 @NgModule({
     declarations: [
         DashboardComponent,
-        DoctoresComponent,
         ProfileComponent
     ],
     imports: [
